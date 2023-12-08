d15cddeaaa6adc1e78df006b771fccd4d75cefc1
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-12-08 14:45:06 +0100

    feat: ovh in prod (#103)
    * feat: ovh in prod
    
    * fix: mentions legales
    
    * fix: mentions legales

--- pages/mentions-legales.tsx
+++ pages/mentions-legales.tsx
@@ -9,39 +9,71 @@ const About: React.FC = () => (
       <div>
         <h2>Éditeur</h2>
         <p>
-        Ce site est édité au sein de l’incubateur de la Fabrique numérique des ministères sociaux par la Direction du numérique des ministères sociaux :
+          Ce site est édité au sein de l’incubateur de la Fabrique numérique des
+          ministères sociaux par la Direction du numérique des ministères
+          sociaux :
         </p>
         <p>
-        Tour Mirabeau<br />
-        39-43 Quai André Citroën<br />
-        75015 Paris<br />
-        Téléphone : 01 40 56 60 00
+          Tour Mirabeau
+          <br />
+          39-43 Quai André Citroën
+          <br />
+          75015 Paris
+          <br />
+          Téléphone : 01 40 56 60 00
         </p>
 
         <h2>Directeur de la publication</h2>
-        <p>Madame Hélène Brisset, Directrice du numérique des ministères sociaux.</p>
+        <p>
+          Madame Anne JeanJean, Directrice du numérique des ministères sociaux.
+        </p>
 
         <h2>Hébergement du site</h2>
         <p>
-        Ce site est hébergé par : <br />
-        Amazon Web Services <br />
-        38 avenue John F. Kennedy <br />
-        L-1855, Luxembourg, ATTN
+          Ce site est hébergé par : <br />
+          [https://www.ovh.com](https://www.ovh.com/) OVH SAS
+          <br />
+          Siège social : 2 rue Kellermann - 59100 Roubaix - France. <br />
         </p>
 
         <h2>Accessibilité</h2>
-        <p>La conformité aux normes d’accessibilité numérique est un objectif ultérieur mais nous tâchons de rendre ce site accessible à toutes et à tous.</p>
+        <p>
+          La conformité aux normes d’accessibilité numérique est un objectif
+          ultérieur mais nous tâchons de rendre ce site accessible à toutes et à
+          tous.
+        </p>
 
         <h2>Signaler un dysfonctionnement</h2>
-        <p>Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site, merci de nous en faire part.</p>
-        <p>Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits.</p>
-        <p>En savoir plus : <br />
-        Pour en savoir plus sur la politique d’accessibilité numérique de l’État : <a href="http://references.modernisation.gouv.fr/accessibilite-numerique">http://references.modernisation.gouv.fr/accessibilite-numerique</a></p>
+        <p>
+          Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder
+          à un contenu ou une fonctionnalité du site, merci de nous en faire
+          part.
+        </p>
+        <p>
+          Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en
+          droit de faire parvenir vos doléances ou une demande de saisine au
+          Défenseur des droits.
+        </p>
+        <p>
+          En savoir plus : <br />
+          Pour en savoir plus sur la politique d’accessibilité numérique de
+          l’État : 
+          <a href="https://references.modernisation.gouv.fr/accessibilite-numerique">
+            https://references.modernisation.gouv.fr/accessibilite-numerique
+          </a>
+        </p>
 
         <h2>Sécurité</h2>
-        <p>Le site est protégé par un certificat électronique, matérialisé pour la grande majorité des navigateurs par un cadenas. Cette protection participe à la confidentialité des échanges.</p>
-        <p>En aucun cas les services associés à la plateforme ne seront à l’origine d’envoi de courriels pour demander la saisie d’informations personnelles.</p>
-
+        <p>
+          Le site est protégé par un certificat électronique, matérialisé pour
+          la grande majorité des navigateurs par un cadenas. Cette protection
+          participe à la confidentialité des échanges.
+        </p>
+        <p>
+          En aucun cas les services associés à la plateforme ne seront à
+          l’origine d’envoi de courriels pour demander la saisie d’informations
+          personnelles.
+        </p>
       </div>
     </div>
   </Page>
